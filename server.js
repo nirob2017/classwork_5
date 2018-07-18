989e29d2113254b507e37d22e6ac232dd61ca5da
Md Monsur Hossain Nirob <nirob726@gmail.com>
2018-07-18 04:44:19 +0000

    classwork_6_and_7

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
-  var http = require('http');
-  var fs = require('fs');
+  // var http = require('http');
+  // var fs = require('fs');
   
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html',function(err,data){
-        if(err){
-            return console.log("file read error");
-        }
+  // var server = http.createServer(function(req, res){
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/html');
+  //   fs.readFile('index.html',function(err,data){
+  //       if(err){
+  //           return console.log("file read error");
+  //       }
     
-    res.end(data);
-  });
+  //   res.end(data);
+  // });
   
-  });
+  // });
+  // server.listen(process.env.PORT, process.env.IP, function(){
+  //   console.log('Server running');
+  // });
+  
+  ////
+var http = require('http');
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server = http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+'/form.html');
+});
+
+app.post('/submit_user',function(req,res)
+{
+    console.log(req.body);
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
